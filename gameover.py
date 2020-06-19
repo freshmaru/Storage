00339d70b15bbce8107156468acbf4a1c7e94468
freshmaru <63366874+freshmaru@users.noreply.github.com>
2020-06-19 22:41:43 +0900

    Update gameover.py

--- gameover.py
+++ gameover.py
@@ -8,7 +8,7 @@ BLACK = (0, 0, 0)
 
 font_name = pg.font.match_font('arial')
 
-
+#문자를 그려주는 함수입니다
 def draw_text(surf, text, font_size, x, y):
     font = pg.font.Font(font_name, font_size)
     text_surface = font.render(text, True, WHITE)
@@ -16,7 +16,9 @@ def draw_text(surf, text, font_size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
-
+#게임 오버 스크린을 출력합니다
+#스페이스바를 눌렀을 시 메인화면으로 돌아갑니다
+#아무 키나 눌렀을시 게임을 다시 시작합니다
 def show_go_screen():
     draw_text(screen, "Game Over", 64, size[0] / 2, size[1] / 4)
     draw_text(screen, "Press space to return", 22, size[0] / 2, size[1] / 2)
